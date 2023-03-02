f9a5808d4c6a28c6c269b13408b0a426dc2bc7cd
Dhanunjay Mamidi <dhanu@Dhanunjays-MacBook-Air.local>
2023-03-02 15:35:15 +0530

    transcript summarizer intial version

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# Interview-Transcript-Summarizer
+# Interview Transcript Summarizer
+
+This is a Python script that uses the GPT-3 API from OpenAI to summarize an interview transcript. The script reads in a transcript from a text file and then generates a summary of each dialogue using GPT-3.
+
+## Requirements
+
+- Python 3.6 or higher
+- OpenAI API key
+- openai Python package
+
+## Installation
+
+- Clone this repository to your local machine.
+- Navigate to the project directory in your terminal.
+- Create a virtual environment using virtualenv: <br>
+
+  ```sh
+  virtualenv venv
+  ```
+
+- Activate the virtual environment: <br>
+
+  ```sh
+  source venv/bin/activate
+  ```
+
+- Install required packages: <br>
+  ```sh
+  pip install -r requirements.txt
+  ```
+
+## Usage
+
+- Add your OpenAI API key to a file named .env in the project directory: <br>
+
+  ```sh
+  OPENAI_API_KEY=<your-api-key>
+  ```
+
+- Create a text file with the transcript you want to summarize.
+- Run the script with the following command: <br>
+
+  ```sh
+  python summarize_transcript.py <input_filename> <output_filename> [--prompt=<prompt>] [--length=<length>]
+  ```
+
+  - `<input_filename>`: The name of the input text transcript file along with the path.
+  - `<output_filename>`: The name of the output that will be generated along with path.
+  - `--prompt=<prompt>` (optional): The prompt to use for GPT-3. Defaults to "Summarize an interview transcript using GPT-3".
+  - `--length=<length>` (optional): The maximum length of the summary in tokens. Defaults to 600.
+
+  ### Example:
+
+  ```sh
+  python summarize_transcript.py transcript.txt summary.txt --prompt="Summarize this interview" --length=500
+  ```
+
+## License
+
+This code is licensed under the MIT License. See the [LICENSE](https://github.com/dhanumamidi/Interview-Transcript-Summarizer/blob/main/LICENSE) file for details.
---
+++ summarize_transcript.py
@@ -0,0 +1,79 @@
+import argparse
+import openai
+import re
+
+default_stop = "<<END>>"
+max_chunk_size = 10000
+
+
+def read_file(path):
+    with open(path, "r") as f:
+        return f.read()
+
+
+def write_file(path, text):
+    with open(path, "w") as f:
+        f.write(text)
+
+
+# Split transcript into chunks of around 6000 characters without splitting a dialogue
+def create_chunks(transcript):
+    dialogues = re.findall(r'([A-Za-z]+: ".*?")(?:\s|$)', transcript)
+    chunks = []
+    chunk = ""
+    for dialogue in dialogues:
+        chunk += dialogue
+        if len(chunk) > 6000:
+            chunks.append(chunk)
+            chunk = ""
+    return chunks
+
+
+def gpt_summarize(prompt, engine="text-davinci-003", temperature=0.5, max_tokens=600, top_p=1, frequency_penalty=0, presence_penalty=0, stop=["<<END>>"], best_of=1):
+    try:
+        response = openai.Completion.create(
+            engine=engine,
+            prompt=prompt,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            top_p=top_p,
+            frequency_penalty=frequency_penalty,
+            presence_penalty=presence_penalty,
+            best_of=best_of,
+            stop=stop,
+        )
+        return response.choices[0].text
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        raise e
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Summarize an interview transcript using GPT-3")
+    parser.add_argument("input_file", type=str, help="Path to input text file")
+    parser.add_argument("output_file", type=str, help="Path to output text file")
+    parser.add_argument(
+        "--prompt", type=str, default="Summarize an interview transcript using GPT-3 by preserving all the statistical and important information", help="Prompt to use for summarization"
+    )
+    parser.add_argument("--length", type=int, default=600, help="Maximum length of summary")
+    args = parser.parse_args()
+
+    input_transcript = read_file(args.input_file)
+    output_summary = ""
+    chunks = create_chunks(input_transcript)
+    try:
+        for chunk in chunks:
+            prompt = args.prompt + "\n" + chunk + "\n" + default_stop
+            output_summary += gpt_summarize(prompt, max_tokens=1000)  # Summarizing each chunk separately to get a initial moderate summary of the transcript
+
+        write_file("detailed_" + args.output_file, output_summary)
+
+        # Summarizing all the chuck summaries together to get a more concise summary
+        prompt = "write very detailed summary of the below text by preserving all the statistical and important information: \n" + output_summary + "\n" + default_stop
+        output_summary = gpt_summarize(prompt, max_tokens=args.length, best_of=3)
+        output_summary = output_summary.strip()
+        output_summary = output_summary.replace("\n", " ")
+        write_file(args.output_file, output_summary)
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        raise e
